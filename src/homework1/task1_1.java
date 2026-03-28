fda05fe516bf1a1024f6be0a6b65d6e7e11a8344
Anastasiya Sudarenko <snastiya@mail.ru>
2026-03-28 09:06:09 +0300

    Добавлены задания из домашки 1

---
+++ src/homework1/task1_1.java
@@ -0,0 +1,17 @@
+package homework1;
+
+import java.util.Arrays;
+
+public class task1_1 {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 10;
+        int c = 5;
+
+        a = 4*(b+c-1)/2;
+
+        System.out.println(a);
+    }
+}
---
+++ src/homework1/task1_6.java
@@ -0,0 +1,21 @@
+
+package homework1;
+
+public class task1_6 {
+
+    public static void main(String[] args) {
+
+    int a = 35;
+    int b = 3;
+
+    int temp; // переставить через временную переменную
+
+    temp = a;
+    a = b;
+    b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
